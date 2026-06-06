e7bc256e3de1f7f1a7427af0a8588f31a98fe78a
Zacarus21 <Zac_21@hotmail.co.uk>
2026-06-06 22:33:21 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-// The service worker caches the app's files so it works offline
-// Think of it as a little background helper that saves a copy of your app
-
 const CACHE_NAME = 'tip-jar-v2';
 
-// These are all the files we want to save for offline use
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// When the app is first installed, save all the files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-// When the app asks for a file, try the cache first
-// If it's not there, go to the internet
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
